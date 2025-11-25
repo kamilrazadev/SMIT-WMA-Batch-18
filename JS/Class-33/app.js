962c913e10370e09fea49518df61cd50aa1530c0
MRMutahir <mutahirkareem820@gmail.com>
2025-11-25 10:51:06 +0500

    Class 33 Added

---
+++ JS/Class-33/app.js
@@ -0,0 +1,78 @@
+// var text =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, perferendis.";
+
+// var segIndex = text.indexOf("amet");
+// var segIndexNotFound = text.indexOf("kamil");
+
+// console.log("segIndex: ", segIndex);
+
+// var text = "To be or not to be.";
+// var segIndex = text.lastIndexOf("be");
+
+// console.log("segIndex: ", segIndex);
+
+// var fullName = "Kamil Raza";
+// var firstChar = fullName.charAt(0);
+// var firstChar = fullName[0];
+
+// console.log("firstChar: ", firstChar);
+
+// var fullName = "Kamil Raza";
+// var lastChar = fullName.charAt(fullName.length - 1);
+
+// console.log("lastChar: ", lastChar);
+
+// var fullName = "Kamil Raza";
+// // Wrong
+// fullName[8] = "a";
+
+// const newName = fullName.replace("a", "z");
+// console.log("fullName: ", fullName);
+// console.log("newName: ", newName);
+
+// var text =
+//   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur consectetur sint alias a amet perspiciatis, ducimus dignissimos dolore reprehenderit amet suscipit tempora nostrum recusandae magni amet labore.";
+
+// var newText = text.replace("amet", "");
+// var newTextReplaceAll = text.replaceAll("amet", "");
+
+// console.log("newText: ", newText);
+// console.log("newTextReplaceAll: ", newTextReplaceAll);
+
+// var scoreAvg = 2.4;
+// var numberOfStars = Math.round(scoreAvg);
+// var numberOfStarsCiel = Math.ceil(scoreAvg);
+// var numberOfStarsFloor = Math.floor(1.999999999);
+
+// console.log("numberOfStars: ", numberOfStars);
+// console.log("numberOfStarsCiel: ", numberOfStarsCiel);
+// console.log("numberOfStarsFloor: ", numberOfStarsFloor);
+
+// var randomNumber = Math.random();
+// var wholeNumber = randomNumber * 100000000000000000;
+
+// console.log("randomNumber: ", randomNumber);
+// console.log("wholeNumber: ", wholeNumber);
+
+// var randomNumber = Math.random();
+// var die = Math.floor(randomNumber * 6 + 1);
+
+// console.log("die: ", die);
+
+// var age = "20";
+// var newAge = +age + +"10";
+
+// console.log("newAge: ", newAge);
+
+console.log(parseInt("1.99999"));
+
+console.log(parseFloat("1.99999"));
+
+console.log(parseFloat("12"));
+
+console.log(Number("1.99999"));
+
+console.log((1.999).toString());
+
+var ratingAvg = "4.8486855494";
+console.log("Your Rating: ", parseFloat(ratingAvg).toFixed(1));
