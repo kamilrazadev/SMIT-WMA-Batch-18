4c9e62046ea5fbcd56455131a313db85d67cf56b
hasanCoder66 <hasanashraf874@gmail.com>
2026-01-13 12:55:52 +0500

    todo app

--- JS/JavaScript-Project/Todo-App/app.js
+++ JS/JavaScript-Project/Todo-App/app.js
@@ -5,43 +5,104 @@
 let inputText = document.getElementById("inputText");
 let addBtn = document.getElementById("addBtn");
 let todoList = document.getElementsByTagName("ul")[0];
+let deleteBtn = document.querySelector(".btn");
+// let todoText = "";
+let editItem = null;
 // let deleteBtn = document.querySelector(".fa-trash")
 
 // console.log(todoList)
 // console.log(inputText)
 // console.log(deleteBtn)
 
-function errorsAlert() {
+function errorsAlert(icon, title, message) {
   Swal.fire({
-    icon: "error",
-    title: "Oops...",
-    text: "Something went wrong!",
-    footer: '<a href="#">Please Enter Input Value</a>',
+    icon: icon,
+    title: title,
+    text: message,
   });
 }
 
+function editHandler(event) {
+  editItem = event.parentElement.parentElement.parentElement;
+  // console.log(editItem)
+
+  todoText = editItem.innerText;
+
+  inputText.value = todoText;
+}
 function addHandler() {
-    
-  // console.log("add handler is working -->", inputText.value)
+  if (editItem !== null) {
+
+    editItem.textContent = inputText.value
+    console.log(editItem)
+    editItem = null
+    inputText.value = ""
+    // editItem.innerText = todoText
+
+  }
+  // let condition = editItem == null;
+  // if (condition) {
+  //   console.log("element mil gaya");
+  // } else {
+  //   editItem.innerText = todoText;
+  // }
+
   if (inputText.value.trim() == "") {
-    errorsAlert();
+    errorsAlert("error", "kuchbi", "Please Enter a Value");
     return;
   }
   todoList.innerHTML += ` <li>
           ${inputText.value}
           <div class="icons">
             <span><i onclick="deleteHandler()" class="fa-solid fa-trash"></i></span>
-            <span><i class="fa-solid fa-pen-to-square"></i></span>
+            <span><i class="fa-solid fa-pen-to-square" onclick="editHandler(this)"></i></span>
           </div>
         </li>`;
 
-        inputText.value = ""
+  inputText.value = "";
 }
 
-function deleteHandler (){
-    event.target.parentElement.parentElement.parentElement.remove()
+function deleteAllHandler() {
+  // console.log("mai chl raha hon ==>")
+  // if(){}
+
+  console.log(todoList.children);
+
+  if (todoList.children.length == 0) {
+    errorsAlert("error", "kuchbi", "Nothing to Delete!");
+    return;
+  }
+  let todoArr = todoList.children;
+
+  // for (let i = 0; i < todoArr.length; i++) {
+
+  //   todoArr[i].remove()
+  //   // i--
+  //   errorsAlert("success", "Complete", "Deleted Successfully")
+  // }
+
+  // while(todoList.children){
+  //   // console.log(todoList.children)
+  // }
+  let i = 0;
+  while (i < todoList.children.length) {
+    todoList.children[i].remove();
+  }
+  console.log("mai agy bi chla ==>");
+}
+
+function deleteHandler() {
+  event.target.parentElement.parentElement.parentElement.remove();
 }
 
 addBtn.addEventListener("click", addHandler);
+deleteBtn.addEventListener("click", deleteAllHandler);
 
-// console.log(deleteBtn)  
+// console.log(deleteBtn)
+
+// [0,1,2]
+
+// arr => 0 remove
+// updated Arr = >  [1,2]
+
+// let i = 1  - 1 0
